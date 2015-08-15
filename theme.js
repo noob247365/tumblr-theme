6f6e65561c728494708ba529a4eec90dc25b9160
noob247 <noob247365@gmail.com>
2015-08-14 21:59:02 -0700

    Fixed tag links
    Make tag links actually go where they're supposed to and be unique...

--- theme.js
+++ theme.js
@@ -99,12 +99,18 @@ $(document).ready(function(){
 	if (root != 'www.tumblr.com') {
   //call json
     var posts = tumblr_api_read.posts;
+    var tags = {};
     //loop through posts
     for (var i = 0; i < posts.length; i++) {
       $('#project-list').append('<li><a href=' + posts[i].url + '>' + posts[i].tags[0] +'</a></li>');
       if(posts[i].tags[1]) {
-      	$('#installation-list').append('<li><a href=' + posts[i].url + '>' + posts[i].tags[1]+ '</a></li>');
+      	tags[posts[i].tags[1]] = true;
       }
+    }
+    // loop through tags
+    var tagUrl = tumblr_api_read.tumblelog.name + '.tumblr.com/tagged/';
+    for(var tag in tags) {
+      	$('#installation-list').append('<li><a href=' + tagUrl + encodeURIComponent(tag) + '>' + tag + '</a></li>');
     }
 
 } else {
